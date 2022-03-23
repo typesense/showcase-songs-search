1b7e29b727033f6b058561ef4bda61a12272bf7a
Jason Bosco <mail@jasonbos.co>
2022-03-23 18:38:18 -0500

    Ability to override hostname

--- src/app.js
+++ src/app.js
@@ -22,16 +22,25 @@ import { SearchClient as TypesenseSearchClient } from 'typesense'; // To get the
 import images from '../images/*.*';
 import STOP_WORDS from './utils/stop_words.json';
 
+// Source: https://stackoverflow.com/a/901144/123545
+const anchorParams = new Proxy(
+  new URLSearchParams(window.location.hash.replace('#', '')),
+  {
+    get: (anchorParams, prop) => anchorParams.get(prop),
+  }
+);
+
 let TYPESENSE_SERVER_CONFIG = {
   apiKey: process.env.TYPESENSE_SEARCH_ONLY_API_KEY, // Be sure to use an API key that only allows searches, in production
   nodes: [
     {
-      host: process.env.TYPESENSE_HOST,
+      host: anchorParams.host ? anchorParams.host : process.env.TYPESENSE_HOST,
       port: process.env.TYPESENSE_PORT,
       protocol: process.env.TYPESENSE_PROTOCOL,
     },
   ],
   numRetries: 8,
+  useServerSideSearchCache: true,
 };
 
 // [2, 3].forEach(i => {
@@ -49,7 +58,9 @@ let TYPESENSE_SERVER_CONFIG = {
 
 if (process.env[`TYPESENSE_HOST_2`]) {
   TYPESENSE_SERVER_CONFIG.nodes.push({
-    host: process.env[`TYPESENSE_HOST_2`],
+    host: anchorParams.host
+      ? anchorParams.host
+      : process.env[`TYPESENSE_HOST_2`],
     port: process.env.TYPESENSE_PORT,
     protocol: process.env.TYPESENSE_PROTOCOL,
   });
@@ -57,7 +68,9 @@ if (process.env[`TYPESENSE_HOST_2`]) {
 
 if (process.env[`TYPESENSE_HOST_3`]) {
   TYPESENSE_SERVER_CONFIG.nodes.push({
-    host: process.env[`TYPESENSE_HOST_3`],
+    host: anchorParams.host
+      ? anchorParams.host
+      : process.env[`TYPESENSE_HOST_3`],
     port: process.env.TYPESENSE_PORT,
     protocol: process.env.TYPESENSE_PROTOCOL,
   });
@@ -65,7 +78,9 @@ if (process.env[`TYPESENSE_HOST_3`]) {
 
 if (process.env[`TYPESENSE_HOST_NEAREST`]) {
   TYPESENSE_SERVER_CONFIG['nearestNode'] = {
-    host: process.env[`TYPESENSE_HOST_NEAREST`],
+    host: anchorParams.host
+      ? anchorParams.host
+      : process.env[`TYPESENSE_HOST_NEAREST`],
     port: process.env.TYPESENSE_PORT,
     protocol: process.env.TYPESENSE_PROTOCOL,
   };
